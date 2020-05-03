968d3a8681aa032cd824fc1c29ad430b5bae98c8
hariharan-tendercuts <hariharan@tendercuts.in>
2020-05-03 21:58:19 +0530

    listen to document scroll and update

--- script.js
+++ script.js
@@ -1,39 +1,86 @@
-let blockedWords = [
-  "corona",
-  "கரோனா",
-  "anxiety",
-  "lockdown",
-  "covid",
-  "covid-19",
-  "virus",
-  "quarantine",
-];
-let hiddenVideosCount = 0;
-// TO-DO should update only on scroll
-setInterval(() => fetchVideos(), 3000);
+let blockedWordsMap = {
+  corona: {
+    key: "corona",
+    count: 0,
+  },
+  கரோனா: {
+    key: "கரோனா",
+    count: 0,
+  },
+  கொரோனா: {
+    key: "கொரோனா",
+    count: 0,
+  },
+  anxiety: {
+    key: "anxiety",
+    count: 0,
+  },
+  lockdown: {
+    key: "lockdown",
+    count: 0,
+  },
+  covid: {
+    key: "covid",
+    count: 0,
+  },
+  "covid-19": {
+    key: "covid",
+    count: 0,
+  },
+  virus: {
+    key: "virus",
+    count: 0,
+  },
+  quarantine: {
+    key: "quarantine",
+    count: 0,
+  },
+};
+let scrollDebouncer;
+let allVideosTitles;
+
+document.onscroll = function () {
+  clearTimeout(scrollDebouncer);
+  scrollDebouncer = setTimeout(() => fetchVideos(), 1000);
+};
+
+fetchVideos();
 
 function fetchVideos() {
-  let allVideosTitles = document.querySelectorAll("#video-title");
-  console.log(`All videos count ${allVideosTitles.length}`);
+  allVideosTitles = document.querySelectorAll("#video-title");
   allVideosTitles.forEach((el) => {
     let videoTitle = el.innerText.toLowerCase();
-    for (let word of blockedWords) {
+    for (let word in blockedWordsMap) {
       if (videoTitle.search(word) === -1) {
         continue;
       }
       let parentEl = el.closest("ytd-rich-item-renderer");
+      if (!parentEl) {
+        break;
+      }
       if (parentEl.classList.contains("ytb-blocked")) {
         continue;
       }
-      blockVideo(parentEl);
+      blockVideo(parentEl, word);
       break;
     }
   });
+  logStats();
 }
 
-function blockVideo(element) {
+function blockVideo(element, word) {
   element.classList.add("ytb-blocked");
-  element.style.opacity = 0;
-  element.style.pointerEvents = "none";
-  console.log(`${++hiddenVideosCount} videos hid`);
+  element.style.opacity = 0; // hids the video without affecting layout
+  element.style.pointerEvents = "none"; // restricts all kind of mouse events
+  blockedWordsMap[word].count++;
+}
+
+function logStats() {
+  console.log(`Videos checked: ${allVideosTitles.length}`);
+  for (let word in blockedWordsMap) {
+    if (blockedWordsMap[word].count === 0) {
+      continue;
+    }
+    console.log(`${word}: ${blockedWordsMap[word].count}`);
+  }
 }
